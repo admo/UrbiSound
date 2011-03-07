f1b7240c7a41c4bc1ae7f983085850f090c55d0a
Adam Oleksy <adam.oleksy@gmail.com>
2011-03-07 12:45:03 +0100

    Change definition of UrbiSound constructor.

--- src/UrbiSound.cpp
+++ src/UrbiSound.cpp
@@ -7,28 +7,31 @@
 
 #include "UrbiSound.h"
 
-UrbiSound::UrbiSound(const std::string&) {
+using namespace urbi;
+
+UrbiSound::UrbiSound(const std::string& name) : UObject(name) {
+    
 }
 
 UrbiSound::~UrbiSound() {
 }
 
 bool UrbiSound::play() {
-    
+    return true;
 }
 bool UrbiSound::play(const std::string&) {
-    
+    return true;
 }
 void UrbiSound::pause() {
-        
+    return;
 }
 bool UrbiSound::openDevice() {
-    
+    return true;
 }
 bool UrbiSound::closeDevice() {
-    
+    return true;
 }
 bool UrbiSound::isDeviceOpened() {
-    
+    return true;
 }
 
