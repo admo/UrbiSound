ee5083cfc5c8ba4fc919d71f396f92bbd4c25dfc
Adam Oleksy <adam.oleksy@gmail.com>
2011-03-08 16:27:43 +0100

    Update definition of the interface. Bind function to the Urbi framework.

--- src/UrbiSound.cpp
+++ src/UrbiSound.cpp
@@ -7,22 +7,28 @@
 
 #include "UrbiSound.h"
 
+#include <iostream>
+
 using namespace urbi;
+using namespace std;
 
 UrbiSound::UrbiSound(const std::string& name) : UObject(name) {
-    
+    //Bind the functions
+    UBindFunction(UrbiSound, play);
+    UBindFunction(UrbiSound, stop);
+    UBindFunction(UrbiSound, openDevice);
+    UBindFunction(UrbiSound, closeDevice);
+    UBindFunction(UrbiSound, isDeviceOpened);
+    UBindFunction(UrbiSound, isPlaying);
 }
 
 UrbiSound::~UrbiSound() {
 }
 
-bool UrbiSound::play() {
-    return true;
-}
 bool UrbiSound::play(const std::string&) {
     return true;
 }
-void UrbiSound::pause() {
+void UrbiSound::stop() {
     return;
 }
 bool UrbiSound::openDevice() {
@@ -32,6 +38,9 @@ bool UrbiSound::closeDevice() {
     return true;
 }
 bool UrbiSound::isDeviceOpened() {
+    return true;
+}
+bool UrbiSound::isPlaying() {
     return true;
 }
 
